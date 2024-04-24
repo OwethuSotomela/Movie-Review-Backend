5b0c1bd1078dbe156315a2e5dbb53acaf737a88c
Owethu Sotomela <owethusotomela@gmail.com>
2024-04-24 15:25:28 +0200

    Added logic

--- src/main/java/dev/owethu/movies/MovieService.java
+++ src/main/java/dev/owethu/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.owethu.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
 
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imbdId){
+        return movieRepository.findMovieByImdbId(imbdId);
     }
 }
